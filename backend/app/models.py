0ad3322dff988990bf92791b4ef6ba7d231a80cf
Paweł Wójcik <pavelec@gmail.com>
2024-01-22 14:50:23 +0100

    add offer model

--- backend/app/models.py
+++ backend/app/models.py
@@ -3,3 +3,15 @@ from django.contrib.auth.models import AbstractUser
  
 class User(AbstractUser):
     pass
+
+
+class Offer(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    url = models.CharField()
+    date_spotted = models.DateTimeField(auto_now_add=True)
+    date_disappeared = models.DateTimeField()
+    brand = models.CharField()
+    model = models.CharField()
+    production_year = models.IntegerField()
+    mileage = models.IntegerField()
+    price = models.IntegerField()
