fe83009911ddaa14f9c08c87a0687cd186a44ade
Paweł Wójcik <pavelec@gmail.com>
2024-01-22 12:23:20 +0100

    add authentication

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
  
 from .serializers import UserSerializer
 from .permissions import UserPermission
 from .models import User
  
 class UserViewSet(ModelViewSet):
+    permission_classes = [UserPermission, IsAuthenticated,]
+
     serializer_class = UserSerializer
     queryset = User.objects.all().order_by("-date_joined")
-    permission_classes = [UserPermission,]
+
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hi there!'}
+        return Response(content)
--- backend/main/urls.py
+++ backend/main/urls.py
@@ -1,31 +1,19 @@
-"""
-URL configuration for main project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
 
 from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
  
 from app.views import UserViewSet
+from app import views
 
 router = routers.DefaultRouter()
-router.register("users", UserViewSet)
+router.register('users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("api/", include(router.urls)),
-    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('hello/', views.HelloView.as_view(), name='hello'),
 ]
