139c31e16ded26c5fd13f3112152a876a7f5ad93
Paweł Wójcik <pavelec@gmail.com>
2024-02-21 10:44:28 +0100

    add email field to serializer

--- backend/app/serializers.py
+++ backend/app/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-            user = User(username=validated_data['username'])
+            user = User(username=validated_data['username'], email=validated_data['email'])
             user.set_password(validated_data['password'])
             user.save()
             return user
