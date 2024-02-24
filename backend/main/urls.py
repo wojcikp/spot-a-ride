09fcc5be1cb70f75b59dc876463186bd0652c042
Paweł Wójcik <pavelec@gmail.com>
2024-02-24 16:02:28 +0100

    remove unused test url

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -17,6 +17,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
-    path('hello/', views.HelloView.as_view(), name='hello'),
     path('user-id/', views.UserId.as_view(), name='user_id'),
 ]
